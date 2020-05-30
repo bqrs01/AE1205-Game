05aa05a00169ac3c403471851a64a249204cac21
Mario Padrón <mario.padron.tardaguila@gmail.com>
2020-05-30 13:38:00 +0200

    A bit of everything

--- src/Game Elements.py
+++ src/Game Elements.py
@@ -51,10 +51,10 @@ class Player(object):
     def draw(self):
         pass
         """I DONT KNOW HOW YOU HAVE TO DO IT TO DRAW IN THIS CODE, BUT TO DRAW THE OBJECT YOU HAVE TO USE:
-        screen.blit(pg.transform.rotate(self.img,slef.angle),(self.x_pos,self.y_pos))"""
+        screen.blit(pg.transform.rotate(self.img,self.angle),(self.x_pos,self.y_pos))"""
 
 
-class Enemy(object):
+class Enemy1(object):
     def __init__(self):
         self.x_pos = 0
         self.y_pos = 0
@@ -73,7 +73,31 @@ class Enemy(object):
 
     def draw(self):
         """ I dont know how to draw it xD, but the same as before:
-        screen.blit(pg.transform.rotate(self.img,slef.angle),(self.x_pos,self.y_pos))"""
+        screen.blit(pg.transform.rotate(self.img,self.angle),(self.x_pos,self.y_pos))"""
+
+
+class Enemy2(object):
+    def __init__(self):
+        self.x_pos = 0
+        self.y_pos = 0
+        self.vel = 2  # we have to do that the velocity modulus is constant, but depending in the position wrt
+        # the Player x and y vel variate, always summing a modulus of 2
+        self.x_vel = 0
+        self.y_vel = 0
+        self.angle = 0
+        self.img = pg.image.load("./images/greenplain.png")
+        self.img = pg.transform.scale(self.img, (32, 32))
+
+    def movement(self, playerx, playery):
+        self.angle = atan2(-(playery - self.y_pos), (playerx - self.x_pos)) * 180 / pi
+        self.x_vel = cos(self.angle * pi / 180) * self.vel
+        self.y_vel = sin(self.angle * pi / 180) * self.vel
+
+    def draw(self):
+        """ I dont know how to draw it xD, but the same as before:
+        screen.blit(pg.transform.rotate(self.img,self.angle),(self.x_pos,self.y_pos))"""
+
+
 class Bullet(object):
     def __init__(self):
         # Maybe change this to the position of the vehicle
@@ -81,4 +105,18 @@ class Bullet(object):
         self.y_pos = 0
         self.vel = 2
         self.x_vel = 0
-        self.y_vel= 0
+        self.y_vel = 0
+        self.img = pg.image.load("")
+        self.img = pg.transform.scale(self.img, (16, 16))
+
+    def movement(self, playerx, playery, angle):
+        self.x_pos = playerx  # We need to change this so it appears in the middle of the obj in front
+        self.y_pos = playery  # Same
+        self.x_vel = self.vel * cos(angle)
+        self.y_vel = self.vel * sin(angle)
+        # We also need to define the impact, or when it reaches the end of the screen
+
+    def draw(self):
+        """ We also need to draw this, as
+        creen.blit(pg.transform.rotate(self.img,slef.angle),(self.x_pos,self.y_pos))"""
+        pass

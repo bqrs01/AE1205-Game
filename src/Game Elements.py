af1c300083d2c36a98fbcd496500190dd7c2d1bc
Mario Padrón <mario.padron.tardaguila@gmail.com>
2020-05-30 11:05:36 +0200

    movement

--- src/Game Elements.py
+++ src/Game Elements.py
@@ -1,7 +1,9 @@
 # Im trying this file to try to declare some objects and functions
 import pygame as pg
+from math import sqrt
 
-#HERE IM JUST SETTING THE VALUES, I DONT NOW HOW TO ACTUALLY SHOW IT IN THE SCREEN AND APPLY THE CHANGES :)
+
+# HERE IM JUST SETTING THE VALUES, I DONT NOW HOW TO ACTUALLY SHOW IT IN THE SCREEN AND APPLY THE CHANGES :)
 
 class Player(object):
     def __init__(self):
@@ -13,20 +15,25 @@ class Player(object):
         self.angle = 0
         self.img = pg.image.load("./images/whiteplain.png")
 
-    def movement(self,event):
+    def movement(self, event):
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_RIGHT:
+            if event.key == pg.K_d:
                 self.x_vel = 2
-            elif event.key == pg.K_LEFT:
+            elif event.key == pg.K_a:
                 self.x_vel = -2
-            elif event.key == pg.K_UP:
+            elif event.key == pg.K_w:
                 self.y_vel = 2
-            elif event.key == pg.K_DOWN:
+            elif event.key == pg.K_s:
                 self.y_vel = -2
             elif event.key == pg.SPACE:
-                pass # Shoot bullet
+                pass  # Shoot bullet
         if event.type == pg.KEYUP:
             if event.key == pg.K_LEFT or event.type == pg.K_RIGHT:
                 self.x_vel = 0
             if event.key == pg.K_UP or event.type == pg.K_DOWN:
                 self.y_vel = 0
+
+    def rotate(self, event):
+        if event.type == pg.MOUSEMOTION:
+            mousex, mousey = event.pos
+            direction_vector = (abs(self.x_pos - mousex), abs(self.y_pos - mousey))

8d097656b0e250c99b32be0f23afe8e63e637e4a
Mario Padrón <mario.padron.tardaguila@gmail.com>
2020-05-30 10:43:20 +0200

    movement

--- src/Game Elements.py
+++ src/Game Elements.py
@@ -1,6 +1,7 @@
 # Im trying this file to try to declare some objects and functions
 import pygame as pg
 
+#HERE IM JUST SETTING THE VALUES, I DONT NOW HOW TO ACTUALLY SHOW IT IN THE SCREEN AND APPLY THE CHANGES :)
 
 class Player(object):
     def __init__(self):
@@ -13,7 +14,7 @@ class Player(object):
         self.img = pg.image.load("./images/whiteplain.png")
 
     def movement(self,event):
-        if event.type = pg.KEYDOWN:
+        if event.type == pg.KEYDOWN:
             if event.key == pg.K_RIGHT:
                 self.x_vel = 2
             elif event.key == pg.K_LEFT:
@@ -24,3 +25,8 @@ class Player(object):
                 self.y_vel = -2
             elif event.key == pg.SPACE:
                 pass # Shoot bullet
+        if event.type == pg.KEYUP:
+            if event.key == pg.K_LEFT or event.type == pg.K_RIGHT:
+                self.x_vel = 0
+            if event.key == pg.K_UP or event.type == pg.K_DOWN:
+                self.y_vel = 0

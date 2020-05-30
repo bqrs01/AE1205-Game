c8360791d677e465271f02d04cd0666170f76587
Mario Padrón <mario.padron.tardaguila@gmail.com>
2020-05-30 10:33:10 +0200

    Player info

--- src/Game Elements.py
+++ src/Game Elements.py
@@ -1,3 +1,14 @@
 # Im trying this file to try to declare some objects and functions
+import pygame as pg
+
 
 class Player(object):
+    def __init__(self):
+        # Position and angle
+        self.x_pos = 0
+        self.y_pos = 0
+        self.x_vel = 0
+        self.y_vel = 0
+        self.angle = 0
+        self.img = pg.image.load("./images/whiteplain.png")
+
